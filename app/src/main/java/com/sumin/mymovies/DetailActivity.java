ecc09c8677f7e39d6cf97885a8365614fcbc482a
suminfx <sumin931@yandex.ru>
2019-01-08 12:51:38 +0300

    Initial commit

---
+++ app/src/main/java/com/sumin/mymovies/DetailActivity.java
@@ -0,0 +1,106 @@
+package com.sumin.mymovies;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+import com.sumin.mymovies.data.FavouriteMovie;
+import com.sumin.mymovies.data.MainViewModel;
+import com.sumin.mymovies.data.Movie;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private ImageView imageViewAddToFavourite;
+    private ImageView imageViewBigPoster;
+    private TextView textViewTitle;
+    private TextView textViewOriginalTitle;
+    private TextView textViewRating;
+    private TextView textViewReleaseDate;
+    private TextView textViewOverview;
+
+    private int id;
+    private Movie movie;
+    private FavouriteMovie favouriteMovie;
+
+    private MainViewModel viewModel;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.itemMain:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.itemFavourite:
+                Intent intentToFavourite = new Intent(this, FavouriteActivity.class);
+                startActivity(intentToFavourite);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        imageViewBigPoster = findViewById(R.id.imageViewBigPoster);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewOriginalTitle = findViewById(R.id.textViewOriginalTitle);
+        textViewRating = findViewById(R.id.textViewRating);
+        textViewReleaseDate = findViewById(R.id.textViewReleaseDate);
+        textViewOverview = findViewById(R.id.textViewOverview);
+        imageViewAddToFavourite = findViewById(R.id.imageViewAddToFavourite);
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("id")) {
+            id = intent.getIntExtra("id", -1);
+        } else {
+            finish();
+        }
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        movie = viewModel.getMovieById(id);
+        Picasso.get().load(movie.getBigPosterPath()).into(imageViewBigPoster);
+        textViewTitle.setText(movie.getTitle());
+        textViewOriginalTitle.setText(movie.getOriginalTitle());
+        textViewOverview.setText(movie.getOverview());
+        textViewReleaseDate.setText(movie.getReleaseDate());
+        textViewRating.setText(Double.toString(movie.getVoteAverage()));
+        setFavourite();
+    }
+
+    public void onClickChangeFavourite(View view) {
+        if (favouriteMovie == null) {
+            viewModel.insertFavouriteMovie(new FavouriteMovie(movie));
+            Toast.makeText(this, R.string.add_to_favourite, Toast.LENGTH_SHORT).show();
+        } else {
+            viewModel.deleteFavouriteMovie(favouriteMovie);
+            Toast.makeText(this, R.string.remove_from_favourite, Toast.LENGTH_SHORT).show();
+        }
+        setFavourite();
+    }
+
+    private void setFavourite() {
+        favouriteMovie = viewModel.getFavouriteMovieById(id);
+        if (favouriteMovie == null) {
+            imageViewAddToFavourite.setImageResource(R.drawable.favourite_add_to);
+        } else {
+            imageViewAddToFavourite.setImageResource(R.drawable.favourite_remove);
+        }
+    }
+}
