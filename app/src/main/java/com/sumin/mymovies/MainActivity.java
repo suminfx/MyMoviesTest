acbe1482d7adb237799d95beaafec28188b32cf9
suminfx <sumin931@yandex.ru>
2019-01-17 11:10:42 +0300

    Решена задача №1 (количество колонок)

--- app/src/main/java/com/sumin/mymovies/MainActivity.java
+++ app/src/main/java/com/sumin/mymovies/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int width = (int) (displayMetrics.widthPixels / displayMetrics.density);
-        return width / 185 > 2 ? width / 185 : 2;
+        return width / 185 > 3 ? width / 185 : 3;
     }
 
     @Override
